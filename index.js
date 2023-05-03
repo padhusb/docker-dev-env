8d041e639019bc131e79621010c89652dd50fba1
Padhu <padhusb@gmail.com>
2023-05-03 04:06:43 +0000

    New addition from container

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end("Hi there!");
+  res.end("Hi there! New message added.");
 });
 
 server.listen(port, hostname, () => {
